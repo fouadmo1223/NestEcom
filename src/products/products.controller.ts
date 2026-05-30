0dcc26589174c20b86a3dedcab40045ee880e6af
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-05-30 18:03:54 +0300

    create product route

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 
 
 @Controller('products')
@@ -6,5 +6,10 @@ export class ProductsController {
     @Get()
     getAll(){
         return [{id: 1, name: 'Product 1'}, {id: 2, name: 'Product 2'}, {id: 3, name: 'Product 3'}]
+    }
+
+    @Post()
+    create(){
+        return 'create product'
     }
 }
