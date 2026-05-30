4edc7ddce9bada9b25b45a58388d5ad145c4ae8f
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-05-30 18:05:51 +0300

    save products in memory

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,9 +3,12 @@ import { Controller, Get, Post } from "@nestjs/common";
 
 @Controller('products')
 export class ProductsController {
+
+    private products = [{id: 1, name: 'Product 1'}, {id: 2, name: 'Product 2'}, {id: 3, name: 'Product 3'}]
+
     @Get()
     getAll(){
-        return [{id: 1, name: 'Product 1'}, {id: 2, name: 'Product 2'}, {id: 3, name: 'Product 3'}]
+        return this.products
     }
 
     @Post()
