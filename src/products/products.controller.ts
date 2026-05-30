eac484993188aee2d9c2e57b93c6f20ac3c2e595
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-05-30 18:10:26 +0300

    craete route

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+
+
+type productType = {
+    id?: number,
+    name: string,
+    price: number
+}
+
 
 
 @Controller('products')
 export class ProductsController {
 
-    private products = [{id: 1, name: 'Product 1'}, {id: 2, name: 'Product 2'}, {id: 3, name: 'Product 3'}]
+    private products = [
+        {id: 1, name: 'Product 1', price: 99.99},
+        {id: 2, name: 'Product 2', price: 149.99},
+        {id: 3, name: 'Product 3', price: 199.99}
+    ]
 
     @Get()
     getAll(){
@@ -12,7 +24,7 @@ export class ProductsController {
     }
 
     @Post()
-    create(){
-        return 'create product'
+    create(@Body() product: productType){
+        return product
     }
 }
