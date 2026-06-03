7ec7359505ae37439e0a713affdbf8af74f75ca0
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-06-03 18:33:11 +0300

    make errors apear as aray of objects

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      exceptionFactory: (errors) =>
+        new BadRequestException(
+          errors.map((e) => ({
+            key: e.property,
+            message: Object.values(e.constraints ?? {}).join(', '),
+          })),
+        ),
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
