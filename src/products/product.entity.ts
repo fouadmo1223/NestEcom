b56b5e0283b644d24114ebd0f5a345f80ef9a21e
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-06-05 17:54:20 +0300

    edit product entity

--- src/products/product.entity.ts
+++ src/products/product.entity.ts
@@ -12,7 +12,7 @@ export class Product {
     price!: number;
 
     @Column({ nullable: true })
-    description!: string;
+    description!: string | null;
 
     @CreateDateColumn()
     createdAt!: Date;
