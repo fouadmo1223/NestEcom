f54bca0c6c300db793cc37e61d6b32a8277bfd37
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-06-05 22:44:15 +0300

    edit user  entite

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,6 +1,11 @@
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { Review } from '../reviews/review.entity';
 
+export enum UserType {
+    ADMIN = 'admin',
+    USER = 'user',
+}
+
 @Entity({ name: 'users' })
 export class User {
     @PrimaryGeneratedColumn()
@@ -14,6 +19,12 @@ export class User {
 
     @Column()
     password!: string;
+
+    @Column({ type: 'enum', enum: UserType, default: UserType.USER })
+    userType!: UserType;
+
+    @Column({ default: false })
+    isAccountVerified!: boolean;
 
     @OneToMany(() => Review, (review) => review.user)
     reviews!: Review[];
