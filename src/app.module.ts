ebfd47d50463aab8a73b676cd2ad8216893de1e9
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-06-05 17:27:09 +0300

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-   imports: [ProductsModule, ReviewsModule, UsersModule],
+   imports: [ProductsModule, ReviewsModule, UsersModule,TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '258258',
+      database: 'nestjs',
+      entities: [ProductsModule, ReviewsModule, UsersModule],
+      synchronize: true,
+   })],
    controllers: [],
    providers: [],
 })
